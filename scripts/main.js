12d6644b2014eb2c26703a48c6a7365fd56eff8d
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-10-10 17:26:32 +0300

    Add logic for checkWinningCondition

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 ////////////// GAME BOARD COORDINATES
-/// 1 2 3 
-/// 4 5 6
-/// 7 8 9
+/// 0 1 2
+/// 3 4 5
+/// 6 7 8
 
 // whole game is tucked away in a IIFE
 const game = (function () {
@@ -13,9 +13,9 @@ const game = (function () {
         // filled with null in the beginning, to suggest being empty on purpose
         let board = [null, null, null, null, null, null, null, null, null];
 
-        let nextTurnBy = player1;
+        let currentTurnBy = player1;
 
-        return {player1, player2, board, nextTurnBy};
+        return {player1, player2, board, currentTurnBy};
     }
 
     // player only needs name and marker; 
@@ -29,34 +29,49 @@ const game = (function () {
             console.log("Field already blocked!")
             return;
         } else {
-            gameStatus.board[position] = gameStatus.nextTurnBy.marker;
+            gameStatus.board[position] = gameStatus.currentTurnBy.marker;
             console.log(gameStatus.board);
             console.log("Marker placed");
+            gameFlow();
         }
         return;
-        // if (gameStatus.board[position] === null) {
-        //     gameStatus.board[position] = gameStatus.nextTurnBy.marker;
-        //     console.log(gameStatus.board);
-        //     console.log("Marker placed")
-        // }
-        // else {
-        //     console.log("Field already blocked");
-        // }
-        // return gameStatus;
     }
 
     function gameFlow() {
-        updateBoard();
-        checkWinningCondition();
-        announceWinner();
+        // updateBoard();
+        winner = checkWinningCondition();
+        if (winner !== null) {
+            announceWinner(winner)
+        } else {
+            if (gameStatus.currentTurnBy === gameStatus.player1) {
+                gameStatus.currentTurnBy = gameStatus.player2;
+            } else if (gameStatus.currentTurnBy === gameStatus.player2) {
+                gameStatus.currentTurnBy = gameStatus.player1
+            }
+        }
     }
 
-            function updateBoard() {
-                console.log("Update board");
-            }
+            // function updateBoard() {
+            //     console.log("Update board");
+            // }
 
             function checkWinningCondition() {
                 console.log("Check winning condition");
+                let m = gameStatus.currentTurnBy.marker;
+                let b = gameStatus.board;
+                // check horizontal, vertical and diagonal winning conditions
+                if (   (b[0] === m && b[1] === m && b[2] === m) 
+                    || (b[3] === m && b[4] === m && b[5] === m) 
+                    || (b[6] === m && b[7] === m && b[8] === m) 
+                    || (b[0] === m && b[3] === m && b[6] === m)
+                    || (b[1] === m && b[4] === m && b[7] === m) 
+                    || (b[2] === m && b[5] === m && b[8] === m) 
+                    || (b[0] === m && b[4] === m && b[8] === m) 
+                    || (b[2] === m && b[4] === m && b[6] === m)) {
+                        return gameStatus.currentTurnBy;
+                } else {
+                        return null;
+                }
             }
 
             function announceWinner() {
